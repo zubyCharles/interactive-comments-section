7d6257a26f1299425cd9b277e5c4dc60166c1a41
zubyCharles <zubecharles@gmail.com>
2022-07-05 10:54:20 +0100

    styled comment input

--- src/components/CommentsSection.js
+++ src/components/CommentsSection.js
@@ -27,20 +27,20 @@ const CommentsList = () => {
   };
 
   return (
-    <main className="lg:w-6/12 lg:my-0 lg:mx-auto">
+    <main className="w-full lg:w-6/12 lg:my-0 lg:mx-auto">
       <div className="">
         {comments.map((comment) => (
           <Comment commentObject={comment} />
         ))}
       </div>
       <div
-        className={`bg-white p-4 pb-16 min-w-[90%] mt-8 mb-16 mx-auto rounded`}
+        className={`bg-white p-4 pb-16 w-[90%] mt-8 mb-16 mx-auto rounded-lg`}
       >
         <div className="avatar w-[9%] lg:w-[4.5%] translate-y-[9rem] lg:translate-y-[2.8rem] lg:mr-24">
           <img src="images/image-juliusomo.png" alt="user avatar" />
         </div>
         <textarea
-          className="w-full lg:max-w-[80%] lg:translate-x-12 pt-2 pb-8 px-4 border-2 rounded"
+          className="w-full lg:max-w-[80%] lg:translate-x-12 pt-2 pb-8 px-4 border-2 rounded-lg"
           ref={commentInputRef}
           placeholder="Add a comment..."
         />
